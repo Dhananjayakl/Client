55ce3f2928adfd314742edaba09dda9509f99668
Dhananjayakl <dhananjaya.kolpe@gmail.com>
2025-04-14 10:58:09 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-
+#$Helloooo????
 import { resolve } from "path";
 
 export default defineConfig({
